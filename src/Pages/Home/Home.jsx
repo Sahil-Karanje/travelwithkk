0b8589f02b382e4480444a1322f78053415a1860
Sahil-Karnaje <sahilkaranje58@gmail.com>
2023-09-07 13:10:59 +0530

    banner ready

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,6 +7,9 @@ const Home = () => {
     <div>
       <section id="home_banner">
         <video src={taj} loop muted autoPlay></video>
+        <div className="black_mask">
+          <h1>Travel <span style={{color:"orange"}}>IN</span><span>D</span><span style={{color:"green"}}>IA</span> With <span style={{color:"orange"}}>KK</span></h1>
+        </div>
       </section>
     </div>
   )
