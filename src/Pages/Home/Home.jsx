74a527c1068f54c59737f9a642b6151375c05fbf
Sahil-Karnaje <sahilkaranje58@gmail.com>
2023-09-07 13:38:42 +0530

    text edited

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Home.css'
 import taj from '../../Assets/Videos/taj.mp4'
+import { FaCrown } from 'react-icons/fa';
 
 const Home = () => {
   return (
@@ -8,7 +9,8 @@ const Home = () => {
       <section id="home_banner">
         <video src={taj} loop muted autoPlay></video>
         <div className="black_mask">
-          <h1>Travel <span style={{color:"orange"}}>IN</span><span>D</span><span style={{color:"green"}}>IA</span> With <span style={{color:"orange"}}>KK</span></h1>
+          {/* <h1>Travel <span style={{color:"orange"}}>IN</span><span>D</span><span style={{color:"green"}}>IA</span> With <span>KK</span>  </h1> */}
+          <h1>Travel INDIA with KK <span className='crown'><FaCrown/></span> </h1>
         </div>
       </section>
     </div>
